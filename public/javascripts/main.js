9ff0d5a1b9afeab3149e96dfa9e609f9c33f2025
Stijn <stijn.brysbaert@ugent.be>
2020-11-21 10:47:12 +0100

    Add frontend files

---
+++ public/javascripts/main.js
@@ -0,0 +1,13 @@
+// const leaflet = require('../../node_modules/leaflet');
+// initialize the map
+var map = L.map(document.getElementById("map")).setView([50.846684, 4.352585], 8);
+
+// load a tile layer
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: 'pk.eyJ1Ijoic2JyeXNiYWUiLCJhIjoiY2tocWVqMWdlMDhsNzJzdDB1eXlxb2FsMiJ9.7y2XQ3jh4gmSBhq5BkaWuw'
+  }).addTo(map);
