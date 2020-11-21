e62bf0085759bd5684a84e4b9767be7491399a9b
Maarten Brysbaert <maarten.brysbaert@qite.be>
2020-11-21 11:17:43 +0100

    Init map logic

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,13 +1,26 @@
-// const leaflet = require('../../node_modules/leaflet');
-// initialize the map
-var map = L.map(document.getElementById("map")).setView([50.846684, 4.352585], 8);
+"use strict";
 
-// load a tile layer
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  maxZoom: 18,
-  id: 'mapbox/streets-v11',
-  tileSize: 512,
-  zoomOffset: -1,
-  accessToken: 'pk.eyJ1Ijoic2JyeXNiYWUiLCJhIjoiY2tocWVqMWdlMDhsNzJzdDB1eXlxb2FsMiJ9.7y2XQ3jh4gmSBhq5BkaWuw'
-  }).addTo(map);
+// Initialize the map
+const initMap = function () {
+  // Get map element
+  const mapElement = document.getElementById("map");
+  
+  // Create map
+  const map = L.map(mapElement).setView([50.846684, 4.352585], 8);
+
+  // Load a tile layer
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: "mapbox/streets-v11",
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: "pk.eyJ1Ijoic2JyeXNiYWUiLCJhIjoiY2tocWVqMWdlMDhsNzJzdDB1eXlxb2FsMiJ9.7y2XQ3jh4gmSBhq5BkaWuw",
+    }
+  ).addTo(map);
+};
+
+// Self invoking function to start main
+(function() {
+  window.addEventListener("DOMContentLoaded", initMap); // document.getElementById will return null if called before DOM loaded
+})()
